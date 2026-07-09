055460781c54a3e3cebe2a21c6622c15f21e433f
Aozora. <walkerfrost931@gmail.com>
2026-07-09 11:36:31 +0700

    Update index-B.js

--- contoh-b/index-B.js
+++ contoh-b/index-B.js
@@ -92,7 +92,7 @@ function start() {
     
 }
 
-const tanggalUltah = new Date(2026, 12, 19); 
+const tanggalUltah = new Date(2026, 5, 19); 
 
 function hitungMundur() {
     const sekarang = new Date();
